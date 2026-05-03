f690246346eb9ebccaded458f4e2540e219e2601
Dikshya Thapa <dikshyatha4258@gmail.com>
2026-05-03 13:30:22 +0545

    Simplify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "docs",
-  turbopack: {
-    root: __dirname,
-  },
 };
 
 export default nextConfig;
