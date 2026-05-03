23634090009159f7163e79a9a506f892385d059e
Dikshya Thapa <dikshyatha4258@gmail.com>
2026-05-03 13:19:02 +0545

    Fix GitHub pages missing assets with .nojekyll

--- docs/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JsOAz9lkwm84UMOhhjsyNqTLCeR3N/uAp6IYJRY0ROU=\"\n}"
--- docs/dev/static/chunks/76489_next_dist_client_components_builtin_global-error_382bc36b.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/76489_next_dist_bc0fee18._.js"
-],
-    source: "dynamic"
-});
